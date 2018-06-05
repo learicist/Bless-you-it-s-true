55a46f99ac619c21658dcee4862091a4fc7c3a84
The Doctor <learicist@gmail.com>
2018-06-05 02:45:07 -0400

    beginnings of functionality

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
+	console.log('hello world');
 	
+	$('#home').click(function () {
+		location.reload();
+	});
+	
+	let navButtons = {
+		'about': $('#aboutSummon'),
+		'gall': $('#gallSummon'),
+		'contact': $('#contactSummon')
+	}
+	
+	$('#topRow').on('click', 'button', function () {
+		$('#welcome').hide(1500);
+		$('#pitch').hide(1700);
+		navButtons[this.id].removeClass('hidden');
+		$('#home').removeClass('hidden');
+	});
 });
