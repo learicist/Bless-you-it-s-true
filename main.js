540828451598c938997f1bf26a0da7490df9fedd
The Doctor <learicist@gmail.com>
2018-06-11 00:55:44 -0400

    div replacement with addClass

--- main.js
+++ main.js
@@ -5,21 +5,35 @@ $(document).ready(function () {
 		location.reload();
 	});
 	
-	let navButtons = {
-		'about': $('#aboutSummon'),
-		'gall': $('#gallSummon'),
-		'contact': $('#formSummon')
-	}
-	
 	$('h1').on('mouseenter', function () {
 		$(this).css('cursor', 'pointer');
 	});
 	
+	let navButtons = {
+		'aboutBtn': $('#aboutSummon'),
+		'gallBtn': $('#gallSummon'),
+		'contactBtn': $('#formSummon')
+	}
+	
 	$('#topRow').on('click', 'button', function () {
 		$('#welcome').hide(1500);
 		$('#pitch').hide(1700);
+		$('#home').removeClass('hidden');		
+		
+		for (let i in navButtons){
+			
+			if (navButtons[i] !== navButtons[this.id]) {
+				
+				navButtons[i].addClass('hidden');
+				//navButtons[i].hide(1500);
+				//USE JQUERY VISUALS INSTEAD
+				
+			}
+		}		
+		
 		navButtons[this.id].removeClass('hidden');
-		$('#home').removeClass('hidden');
+		
+		//console.log(navButtons[this.id].hasClass('hidden'));
 		
 		//add hidden back to currently displayed div when another is clicked
 	});
