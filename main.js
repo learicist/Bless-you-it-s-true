4b32c900f7fce71b7b8184cf45a4e9e7fc3ea303
The Doctor <learicist@gmail.com>
2018-07-07 15:33:00 -0400

    Working with mom 7.7.18

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const navButtons = {
 	'contactBtn': $('#formSummon')
 }
 
+const gallery = [
+	{name: 'gallOne', tag: $('#gallOne'), content: $('#congrats')},
+	{name: 'gallTwo', tag: $('#gallTwo'), content: $('#separate')},
+	{name: 'gallThree', tag: $('#gallThree'), content: $('#apology')},
+	{name: 'gallFour', tag: $('#gallFour'), content: $('#love')},
+	{name: 'gallFive', tag: $('#gallFive'), content: $('#connect')},
+	{name: 'gallSix', tag: $('#gallSix'), content: $('#in_laws')},
+	{name: 'gallSeven', tag: $('#gallSeven'), content: $('#death')},
+];
+
 $(document).ready(function () {
 	console.log('hello mom');
 	$('#home').click(function () {
@@ -20,10 +30,10 @@ $(document).ready(function () {
 	});
 	
 	$('#topRow').on('click', 'button', function () {
-		$('#welcome').hide(400);
-		$('#oscarQuote').hide(400);
+		$('#welcome').addClass('hidden');
+		$('#oscarQuote').addClass('hidden');
 		$('#home').removeClass('hidden');
-		//console.log(this.id);
+
 		if (this.id == 'aboutBtn') {
 			$('#quotes').removeClass('hidden');
 			$('#bergQuote').removeClass('hidden');
@@ -31,15 +41,50 @@ $(document).ready(function () {
 			$('#quotes').addClass('hidden');
 		} 
 		
+		//hide non-clicked items
 		for (let i in navButtons){
 			if (navButtons[i] !== navButtons[this.id]) {
 				navButtons[i].addClass('hidden');
-				//navButtons[i].fadeOut(1100);
-				//navButtons[i].hide(1500);
-				//USE JQUERY VISUALS INSTEAD
 			}
 		}		
 		
 		navButtons[this.id].removeClass('hidden');
+	});
+	
+	let curr, flag = true;
+	$('.gallery').on({
+		'mouseenter': function () {
+			$(this).css('cursor', 'pointer');
+		},
+		'click': function () {
+			curr = this.id;
+			if (flag == true) {
+				flag = false;
+				for (let i in gallery) {
+					if (curr == gallery[i].name) {
+						gallery[i].content.removeClass('hidden');
+					} else {
+						gallery[i].tag.addClass('hidden');
+					}					
+				}
+			} else {
+				flag = true;
+				for (let i in gallery) {
+					if (curr == gallery[i].name) {
+						gallery[i].content.addClass('hidden');
+					} else if (curr !== gallery[i].name) {
+						gallery[i].tag.removeClass('hidden');
+					}
+				}
+			}
+		}
+	});
+	
+	$('#begin').on('click', function () {
+		if ($('#customer').hasClass('hidden')) {
+			$('#customer').removeClass('hidden');
+		} else {
+			$('#customer').addClass('hidden');
+		}
 	});
 });
